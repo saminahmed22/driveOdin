8ddd1fbddeac5f67b4e011e7c1071f90909c90b3
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-01 02:34:22 +0600

    feat(password): Add password hashing before inserting to db

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,21 @@
 import { prisma } from "../lib/prisma.js";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export async function registerUser(req, res) {
   const userSubmittedData = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     username: req.body.username,
-    password_hash: req.body.password,
+    password_hash: await hashPassword(req.body.password),
   };
+
   try {
     const user = await prisma.user.create({
       data: userSubmittedData,
     });
-    console.log("Created user:", user);
-    await prisma.$disconnect();
 
     res.redirect("/");
   } catch (error) {
     console.error(error);
-    await prisma.$disconnect();
-    process.exit(1);
   }
 }
