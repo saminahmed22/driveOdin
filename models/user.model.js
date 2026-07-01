d2a45b792b5b64998a9ff484139733e78ddc6d9b
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-01 22:04:20 +0600

    feat(user-model): Add function to find user form DB

--- models/user.model.js
+++ models/user.model.js
@@ -19,3 +19,20 @@ export async function registerUser(req, res) {
     console.error(error);
   }
 }
+
+export async function findUser({ id, username }) {
+  if (!id && !username) {
+    throw new Error("No ID or username has been provided.");
+  }
+
+  try {
+    const user = await prisma.user.findFirst(
+      id ? { where: { id } } : { where: { username } },
+    );
+
+    return user;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
