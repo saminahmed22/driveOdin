e9597dd78564b3edb5863aee81ff39e1f7fd29cf
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-04 00:47:11 +0600

    feat(error): Add objects to contain error messages

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,19 @@
 export async function renderLoginPage(req, res) {
-  res.render("authPage", { authMode: "login" });
+  const passportErrors = req?.flash("error");
+  // const formValidationErrors = validationResult(req);
+  console.log(passportErrors);
+
+  const hasErrors = passportErrors.length > 0; // || !formValidationErrors.isEmpty();
+
+  res.status(hasErrors ? 400 : 200).render("authPage", {
+    authMode: "login",
+    errorMessages: { passportErrors, validationError: [] },
+  });
 }
 
 export async function renderRegistrationPage(req, res) {
-  res.render("authPage", { authMode: "register" });
+  res.render("authPage", {
+    authMode: "register",
+    errorMessages: { passportError: [], validationError: [] },
+  });
 }
