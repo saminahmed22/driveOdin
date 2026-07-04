abad83e79d0c8ef867e7826f41fb057d9fcb3ad7
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-05 05:03:09 +0600

    feat(upload): Add upload post routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,11 @@ import Router from "express";
 
 // Controllers
 import { renderIndex } from "../controllers/indexController.js";
+import {
+  renderSharePage,
+  uploadImage,
+  uploadPost,
+} from "../controllers/uploadController.js";
 
 // Middlewares
 import { authenticationStatus } from "../middlewares/authenticationStatus.js";
@@ -10,4 +15,15 @@ import { authenticationStatus } from "../middlewares/authenticationStatus.js";
 export const indexRouter = Router();
 
 // Routes
+
+//____get
 indexRouter.get("/", authenticationStatus, renderIndex);
+
+//____post
+indexRouter.post(
+  "/upload",
+  authenticationStatus,
+  uploadImage,
+  uploadPost,
+  renderSharePage,
+);
