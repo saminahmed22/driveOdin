a0087ced21d32c12fe401530fbfef1dc89ee7dc5
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-05 22:58:38 +0600

    feat(download-page): Add function to render download page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,23 @@
-export async function renderIndex(req, res) {
+export function renderIndex(req, res) {
   res.render("index", {
     uploadData: null,
-    uploadModalOpen: false,
+    modalOpen: false,
     modalContent: null,
+  });
+}
+
+export function renderSharePage(req, res) {
+  res.render("index", {
+    data: req,
+    modalOpen: "upload",
+    modalContent: "sharePage",
+  });
+}
+
+export function renderDownloadPage(req, res) {
+  res.render("index", {
+    data: req.post,
+    modalOpen: "download",
+    modalContent: "downloadPage",
   });
 }
