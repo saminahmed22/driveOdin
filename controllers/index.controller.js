6fb743e3eb258b9ee7e3b571aad3377a1f4aa322
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-08 05:06:21 +0600

    feat(folder): Add middlewares for folder creation

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,7 +1,26 @@
-export function renderIndex(req, res) {
+// Modles
+import { createFolderDB, getFolders } from "../models/post.model.js";
+
+export async function renderIndex(req, res) {
+  let folders;
+  if (req.user) {
+    folders = await getFolders(req.user.id);
+  }
+
   res.render("index", {
-    data: {},
+    data: { folders },
     modalOpen: null,
     errors: {},
   });
+}
+
+export async function createFolder(req, res, next) {
+  const folder_name = req.body.folderName;
+  const userId = req.user.id;
+
+  const data = { folder_name, userId };
+
+  await createFolderDB(data);
+
+  res.redirect("/");
 }
