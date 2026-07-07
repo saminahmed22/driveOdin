5969c69fd19a76594f9caee61fca9baa9f31e9f0
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-07 06:19:34 +0600

    feat(auth): Add function to check auth status on the attempt of re-auth.

--- middlewares/authenticationStatus.js
+++ middlewares/authenticationStatus.js
@@ -1,9 +1,26 @@
+// Checks authentication status on every route needed
 export function authenticationStatus(req, res, next) {
   const status = req.isAuthenticated();
 
-  if (status) {
-    return next();
-  } else {
-    return res.redirect("/auth/login");
-  }
+  status ? next() : res.redirect("/auth/login");
+}
+
+// Checks if one account is already logged in or not
+export function authenticationStatusOnReAuth(req, res, next) {
+  const status = req.isAuthenticated();
+
+  status ? res.redirect("/") : next();
+}
+
+// Logs in the user after registration
+export function forceLogin(req, res, next) {
+  req.login(req.user, (error) => {
+    error ? next(error) : res.redirect("/");
+  });
+}
+
+export function forceLogout(req, res, next) {
+  req.logout(req.user, (error) => {
+    error ? next(error) : res.redirect("/auth/login");
+  });
 }
