d73536c3ad65dce98880f397de29889ab3dc477c
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-07 06:17:38 +0600

    feat(util): Add function to add ellipsis to the middle of an overflowed text

---
+++ utils/stringEllipsisMiddle.js
@@ -0,0 +1,14 @@
+// Source - https://stackoverflow.com/a/831657
+// Posted by Stefan Lundström, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-07-06, License - CC BY-SA 4.0
+
+export function middleEllipsis(str, maxLen = 35) {
+  if (str.length > maxLen) {
+    return (
+      str.substring(0, 20) +
+      " ... " +
+      str.substring(str.length - 10, str.length)
+    );
+  }
+  return str;
+}
