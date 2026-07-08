26d0b979e42e36895f4cae15d2b95d1ef3905751
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-09 00:39:53 +0600

    feat(folder): Create folder controller

---
+++ controllers/folder.controller.js
@@ -0,0 +1,41 @@
+// Modles
+import {
+  submitCreateFolder,
+  getFolder,
+  getFolders,
+  submitEditFolder,
+  submitDeleteFolder,
+} from "../models/folder.model.js";
+
+export async function createFolder(req, res, next) {
+  const folder_name = req.body.folderName;
+  const userId = req.user.id;
+
+  const data = { folder_name, userId };
+
+  await submitCreateFolder(data);
+
+  res.redirect("/");
+}
+
+export async function findFolder(req, res, next) {
+  const id = req.params.id;
+
+  const folder = await getFolder(id);
+
+  req.folder = folder;
+
+  next();
+}
+
+export async function editFolder(req, res, next) {
+  return null;
+}
+
+export async function deleteFolder(req, res, next) {
+  return null;
+}
+
+export async function renderFolderPage(data) {
+  return null;
+}
