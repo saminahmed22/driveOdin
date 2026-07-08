6184067fbdae45f0ea306a78c56ea1280cfeda34
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-09 00:35:16 +0600

    feat(folder): Add a router for folder

---
+++ routes/folder.router.js
@@ -0,0 +1,46 @@
+import Router from "express";
+
+export const folderRouter = Router();
+
+// Controllers
+import {
+  createFolder,
+  renderFolderPage,
+  findFolder,
+  editFolder,
+  deleteFolder,
+} from "../controllers/folder.controller.js";
+
+// Middlewares
+import {
+  authenticationStatus,
+  isAuthor,
+} from "../middlewares/authenticationStatus.js";
+
+function redirectToFolderView(req, res, next) {
+  const id = req.params.id;
+
+  res.redirect(`/folder/${id}`);
+}
+
+// Routes
+
+//____get
+folderRouter.post("/:id", findFolder, renderFolderPage);
+
+//____post
+folderRouter.post("/new", authenticationStatus, createFolder);
+folderRouter.post(
+  "/edit/:id",
+  authenticationStatus,
+  isAuthor,
+  editFolder,
+  redirectToFolderView,
+);
+folderRouter.post(
+  "/delete/:id",
+  authenticationStatus,
+  isAuthor,
+  deleteFolder,
+  redirectToFolderView,
+);
