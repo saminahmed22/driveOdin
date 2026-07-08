539e7c9d41a2134ba79b0efc4969b70e86cde437
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-09 00:34:38 +0600

    feat(auth): Add author checker middleware

--- middlewares/authenticationStatus.js
+++ middlewares/authenticationStatus.js
@@ -1,3 +1,7 @@
+// Models
+import { findPostAuthor } from "../models/post.model.js";
+import { findFolderAuthor } from "../models/folder.model.js";
+
 // Checks authentication status on every route needed
 export function authenticationStatus(req, res, next) {
   const status = req.isAuthenticated();
@@ -23,4 +27,27 @@ export function forceLogout(req, res, next) {
   req.logout(req.user, (error) => {
     error ? next(error) : res.redirect("/auth/login");
   });
+}
+
+export async function isAuthor(req, res, next) {
+  req.logout(req.user, (error) => {
+    error ? next(error) : res.redirect("/auth/login");
+  });
+
+  const requester = req.user?.id;
+
+  if (!requester) {
+    res.redirect("/auth/login");
+  }
+
+  const urlArr = req.path.split("/");
+
+  let authorID;
+  if (urlArr.contains("post")) {
+    authorID = await findPostAuthor(req.params.id).id;
+  } else if (urlArr.contains("folder")) {
+    authorID = await findFolderAuthor(req.params.id).id;
+  }
+
+  return requester === authorID;
 }
