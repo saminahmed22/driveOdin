5b3cf80f876c346307f81c8ae786459f5d048dd6
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-11 00:26:33 +0600

    refactor(string-elipsis): Readjust the elipsis position in the middle

--- utils/stringEllipsisMiddle.js
+++ utils/stringEllipsisMiddle.js
@@ -3,10 +3,15 @@
 // Retrieved 2026-07-06, License - CC BY-SA 4.0
 
 export function middleEllipsis(str, maxLen = 35) {
+  if (!str) {
+    throw new Error("No string has been provided.");
+  }
+
   if (str.length > maxLen) {
     return (
-      str.substring(0, 20) + " ... " + str.substring(str.length - 5, str.length)
+      str.substring(0, 13) + "..." + str.substring(str.length - 12, str.length)
     );
   }
+
   return str;
 }
