463445e7aa7976fc7e7f3cce41f793bea24c51c9
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-27 02:01:04 +0600

    shit ton of refactor

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,21 +1,18 @@
 // Models
 import {
-  isPostProtected,
-  decryptPost,
   createPost,
-  getPost,
-  submitEditFile,
-  submitDeleteFile,
+  editPost,
+  deletePost,
+  findPost,
 } from "../models/postModel.js";
 
-import { getFolders } from "../models/folderModel.js";
+import { findFolder } from "../models/folderModel.js";
 
 // Utils
-
 import { generateQR } from "../utils/generateQRcode.js";
 import { middleEllipsis } from "../utils/stringEllipsisMiddle.js";
 import { formatReadableSize } from "../utils/readableFileSize.js";
-import { encryptString } from "../utils/crypto.js";
+import { hashString, compareHash } from "../utils/crypto.js";
 import {
   findPostFromAllData,
   findFolderFromAllData,
@@ -24,108 +21,65 @@ import { reformatPostDataObject } from "../utils/reformatAllDataObject.js";
 import fs from "fs";
 import path from "path";
 
+// Models
+import { isAuthor } from "../models/authModel.js";
+
 // Multer
-import multer from "multer";
+import { uploadImageMulter } from "../lib/multer.js";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/home/samin-ahmed/repos/driveOdin/uploads");
-  },
+//#region Create post
+export async function renderUploadForm(req, res) {
+  res.render("index", {
+    allData: req.allData,
 
-  filename: function (req, file, cb) {
-    //#region File name
-    const originalFileName = file.originalname;
-
-    const file_ext = path.extname(originalFileName);
-
-    const givenFileName = req.body.file_name;
-
-    const file_name = `${givenFileName}${file_ext}`;
-    //#endregion
-
-    cb(null, file_name);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// multer function
-export function uploadImage(req, res, next) {
-  const singleUpload = upload.single("selected_image");
-
-  singleUpload(req, res, (err) => {
-    if (err) {
-      return next(err);
-    }
-
-    next();
+    modalOpen: "uploadForm",
+    errors: {},
   });
 }
 
-export async function uploadPost(req, res, next) {
+export async function handleCreatePostRequest(req, res, next) {
+  await uploadImageMulter(req, res);
+
   const userId = req.user.id;
-  const originalFileName = req.file.originalname;
 
-  //#region File extension type
-  const fileMime = req.file.mimetype;
-
-  const fileMimeSplit = fileMime.split("/");
-
-  const validPhotoMimeTypes = [
-    "jpeg",
-    "png",
-    "gif",
-    "webp",
-    "avif",
-    "svg+xml",
-    "bmp",
-    "x-icon",
-    "vnd.microsoft.icon",
-    "apng",
-    "tiff",
-  ];
-
-  const isValidExt = validPhotoMimeTypes.includes(fileMimeSplit[1]);
-  if (!isValidExt) {
-    throw new Error("Unsupported photo extension.");
-  }
-
-  const file_ext = path.extname(originalFileName);
-  //#endregion
-
-  //#region File name
+  // File name
   const givenFileName = req.body.file_name;
+  const file_ext = path.extname(req.file.originalname);
 
   const file_name = `${givenFileName}${file_ext}`;
-  //#endregion
 
-  //#region File path
-  const isProtected = req.body.postPassword.length > 0;
-
-  const dir = path.dirname(req.file.path);
-  const file_location = `${dir}/${file_name}`;
-
-  const password = req.body.postPassword;
-
-  const location = isProtected
-    ? encryptString(file_location, password)
-    : file_location;
-  //#endregion
-
+  // File size
   const file_size = formatReadableSize(req.file.size);
 
+  // File path
+  const dir = path.dirname(req.file.path);
+  const location = `${dir}/${file_name}`;
+
+  // Password
+  const isProtected = req.body.postPassword.length > 0;
+
+  let passwordHash;
+  if (isProtected) {
+    const password = req.body.postPassword;
+
+    passwordHash = await hashString(password);
+  }
+
+  // Expiry date
   const expires_at = new Date();
   expires_at.setDate(expires_at.getDate() + parseInt(req.body.expiryDate));
 
+  // Folder
   const folderId = req.body.selected_folder;
 
+  //
   const data = {
     userId,
     file_name,
-    isProtected,
-    location,
     file_size,
-    file_ext,
+    location,
+    isProtected,
+    passwordHash,
     expires_at,
     folderId,
   };
@@ -136,18 +90,95 @@ export async function uploadPost(req, res, next) {
 
   next();
 }
+//#endregion
 
-export async function renderUploadForm(req, res) {
+//#region Edit post
+export async function renderFileEditModal(req, res, next) {
+  if (!req.isAuthor) {
+    res.redirect("/");
+
+    return;
+  }
+
+  const postID = req.params.id;
+  const post = findPostFromAllData(postID, req.data);
+
+  post.file_ext = path.extname(post.file_name);
+
   res.render("index", {
-    allData: req.allData,
-
-    modalOpen: "uploadForm",
+    allData: req.data,
+    modalOpen: "editFile",
+    values: { post },
     errors: {},
   });
 }
 
-//#region Share/Download request related function
+export async function handleEditPostRequest(req, res, next) {
+  if (!req.isAuthor) {
+    res.redirect("/");
 
+    return;
+  }
+
+  const post = findPostFromAllData(req.params.id, req.data);
+
+  const file_ext = post.file_ext;
+
+  const file_name = `${req.body.file_name}${file_ext}`;
+
+  const dir = path.dirname(post.location);
+  const newPath = `${dir}/${file_name}`;
+  fs.rename(post.location, newPath, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+  });
+
+  const postID = req.params.id;
+  const userID = req.user.id;
+  const data = { file_name };
+
+  await editPost(postID, userID, data);
+
+  res.redirect("/");
+}
+//#endregion
+
+//#region Delete post
+export async function renderFileDeleteModal(req, res, next) {
+  if (!req.isAuthor) {
+    res.redirect("/");
+
+    return;
+  }
+
+  const post = findPostFromAllData(req.params.id, req.data);
+
+  res.render("index", {
+    allData: req.data,
+    modalOpen: "deleteFile",
+    values: { post },
+    errors: {},
+  });
+}
+
+export async function handleDeletePostRequest(req, res, next) {
+  if (!req.isAuthor) {
+    res.redirect("/");
+
+    return;
+  }
+
+  const postID = req.params.id;
+  const userID = req.user.id;
+
+  await deletePost(postID, userID);
+
+  res.redirect("/");
+}
+//#endregion
+
+//#region Share/Download request related function
 export async function renderDownloadForm(req, res, error) {
   res.render("index", {
     allData: req.data,
@@ -158,11 +189,13 @@ export async function renderDownloadForm(req, res, error) {
 }
 
 export async function getImage(req, res, next) {
+  const authorStatus = req.isAuthor;
+
   const postID = req.params.id;
 
-  const post = req.user
+  const post = authorStatus
     ? findPostFromAllData(postID, req.data)
-    : await getPost(postID);
+    : await findPost(postID);
 
   if (!post) {
     throw new Error(`No post has been found with the post ID: ${postID}`);
@@ -170,26 +203,27 @@ export async function getImage(req, res, next) {
 
   const isProtected = post.isProtected;
 
-  let givenPassword, decryptedPost;
   if (isProtected) {
-    givenPassword = req?.session?.password;
+    console.log(`HERE 1312: ${isProtected}`);
+    console.log(`HERE 1312: ${authorStatus}`);
+    if (!authorStatus) {
+      const givenPassword = req?.session?.password;
 
-    if (!givenPassword) {
-      res.redirect(`/post/passwordRequired/${postID}`);
+      if (!givenPassword) {
+        res.redirect(`/post/passwordRequired/${postID}`);
 
-      return;
-    }
+        return;
+      }
 
-    /*
-    Removes password stored in the session
-    after accessing it the first time
-    */
-    removeDataFromSession(req, res);
+      /*
+      Removes password stored in the session
+      after accessing it the first time
+      */
+      removeDataFromSession(req, res);
 
-    decryptedPost = decryptPost(post, givenPassword);
+      const isGenuine = compareHash(givenPassword, post.passwordHash);
 
-    if (decryptedPost instanceof Error) {
-      if (post.message === "password") {
+      if (!isGenuine) {
         res.redirect(`/post/passwordRequired/${shareCode}`);
 
         return;
@@ -206,28 +240,32 @@ export async function getImage(req, res, next) {
   const qrcode = await generateQR(fullUrl);
   post.qrcode = qrcode;
 
-  post.file_name_short = middleEllipsis(post.file_name);
-
-  if (!req.user) {
-    req.post = post;
-  }
+  req.post = authorStatus ? post : reformatPostDataObject(post);
 
   next();
 }
 
-export async function renderDownloadPage(req, res) {
-  const post = req.user
-    ? findPostFromAllData(req.params.id, req.data)
-    : req.post;
+export async function renderDownloadPage(req, res, next) {
+  const post = req.post;
 
   res.render("index", {
     allData: req?.data,
     modalOpen: "downloadPage",
-    values: { ...post },
-    errors: [post.message],
+    values: { post },
+    errors: [],
   });
 }
 
+export async function renderPasswordRequriedForm(req, res, next) {
+  const postID = req.params.id;
+
+  res.render("index", {
+    allData: req?.data,
+    modalOpen: "passwordRequired",
+    values: { postID },
+    errors: [],
+  });
+}
 //#endregion
 
 //#region
@@ -236,6 +274,7 @@ export async function renderDownloadPage(req, res) {
 // as the router redirects users to post view route on submission
 export function addDataToSession(req, res, next) {
   req.session.password = req.body.postPassword;
+
   req.session.save((err) => {
     if (err) return next(err);
     next();
@@ -248,123 +287,3 @@ export async function removeDataFromSession(req, res) {
   return;
 }
 //#endregion
-
-export async function renderFileEditModal(req, res, next) {
-  const postID = req.params.id;
-  const post = findPostFromAllData(postID, req.data);
-
-  const isProtected = post.isProtected;
-
-  let givenPassword, decryptedPost;
-  if (isProtected) {
-    givenPassword = req?.session?.password;
-
-    if (!givenPassword) {
-      res.redirect(`/post/edit/passwordRequired/${postID}`);
-
-      return;
-    }
-
-    // /*
-    // Removes password stored in the session after 5 minutes
-    // */
-    // setTimeout((req, res) => {
-    //   removeDataFromSession(req, res);
-    // }, 300000);
-
-    decryptedPost = decryptPost(post, givenPassword);
-
-    if (decryptedPost instanceof Error) {
-      if (post.message === "password") {
-        res.redirect(`/post/edit/passwordRequired/${postID}`);
-
-        return;
-      }
-    }
-  }
-
-  res.render("index", {
-    allData: req.data,
-    modalOpen: "editFile",
-    values: { post },
-    errors: {},
-  });
-}
-
-export async function renderEditFilePasswordRequiredModal(req, res, next) {
-  const postID = req.params.id;
-  const post = findPostFromAllData(postID, req.data);
-
-  res.render("index", {
-    allData: req.data,
-    modalOpen: "editPostPasswordRequired",
-    values: { post },
-    errors: {},
-  });
-}
-
-export async function editFile(req, res, next) {
-  const post = findPostFromAllData(req.params.id, req.data);
-
-  const isPostProtected = post.isProtected;
-
-  let givenPassword;
-  if (isPostProtected) {
-    givenPassword = req?.session?.password;
-
-    if (!givenPassword) {
-      res.redirect(`/post/edit/passwordRequired/${postID}`);
-
-      return;
-    }
-
-    /*
-    Removes password stored in the session
-    after accessing it the first time
-    */
-    removeDataFromSession(req, res);
-
-    const decryptedPost = decryptPost(post, givenPassword);
-
-    if (decryptedPost instanceof Error) {
-      if (post.message === "password") {
-        res.redirect(`/post/edit/passwordRequired/${postID}`);
-
-        return;
-      }
-    }
-  }
-
-  const file_ext = post.file_ext;
-
-  const file_name = `${req.body.file_name}${file_ext}`;
-
-  const dir = path.dirname(post.location);
-  const newPath = `${dir}/${file_name}`;
-  fs.rename(post.location, newPath, (err) => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
-
-  await submitEditFile(req.params.id, file_name, newPath);
-
-  res.redirect("/");
-}
-
-export async function renderFileDeleteModal(req, res, next) {
-  const post = findPostFromAllData(req.params.id, req.data);
-
-  res.render("index", {
-    allData: req.data,
-    modalOpen: "deleteFile",
-    values: { post },
-    errors: {},
-  });
-}
-
-export async function deleteFile(req, res, next) {
-  await submitDeleteFile(req.params.id, req.body.file_name);
-
-  res.redirect("/");
-}
