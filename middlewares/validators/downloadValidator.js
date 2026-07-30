470ecc7fd20a87984ad304af6f2890e0a4c8c517
Samin Ahmed <saminahmed1001@gmail.com>
2026-07-31 04:07:31 +0600

    feat(password): Add validator for password

--- middlewares/validators/downloadValidator.js
+++ middlewares/validators/downloadValidator.js
@@ -1,6 +1,7 @@
 import { body } from "express-validator";
 import { findPost } from "../../models/postModel.js";
 import { findFolder } from "../../models/folderModel.js";
+import { compareHash } from "../../utils/crypto.js";
 
 export const validateDownloadForm = [
   body("shareCode")
@@ -37,4 +38,43 @@ export const validateDownloadForm = [
     .bail()
     .trim()
     .escape(),
+
+  body("password").custom(async (value, { req }) => {
+    const contentID = req?.body?.shareCode || req?.params?.id;
+
+    const requestType = req.originalUrl.split("/").includes("post")
+      ? "post"
+      : "folder";
+
+    let content =
+      requestType === "post"
+        ? await findPost(contentID)
+        : await findFolder(contentID);
+
+    if (!content) {
+      return true;
+    }
+
+    const isContentProtected = content.isProtected;
+
+    if (!isContentProtected) {
+      return true;
+    }
+
+    if (value?.length <= 0) {
+      throw new Error(
+        `This ${requestType} is password protected. Please enter the password.`,
+      );
+    } else {
+      const contentHash = content.passwordHash;
+
+      const isGenuine = await compareHash(value, contentHash);
+
+      if (!isGenuine) {
+        throw new Error("Wrong password, please try again.");
+      } else {
+        return true;
+      }
+    }
+  }),
 ];
