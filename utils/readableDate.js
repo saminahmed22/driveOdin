4e4710d80a8f854bad8625babd0d50c51b18be69
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-07 00:53:15 +0600

    refactor: Remove .locale for zone

--- utils/readableDate.js
+++ utils/readableDate.js
@@ -1,5 +1,5 @@
 export function formatReadableDate(isoString) {
-  const date = new Date(isoString).toLocaleString()
+  const date = new Date(isoString);
 
   const formatter = new Intl.DateTimeFormat("en-GB", {
     day: "2-digit",
